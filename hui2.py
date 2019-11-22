4e8acb165bc1095b99f1228f44685ecee1e05cee
RustKnight7 <57863102+RustKnight7@users.noreply.github.com>
2019-11-22 20:27:44 +0300

    Add files via upload

--- hui2.py
+++ hui2.py
@@ -1,22 +1,20 @@
 import telebot
 import random
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
 import time, re
-import winsound
-import pyautogui
 import time
 import pyowm
-import pickle
 import requests
 from mqtt import *
 import datetime
 import reader
+import os
+from dotenv import load_dotenv
 
-commandlist = {'/start': 'start_message(message)', '/help' : 'help_message(message)', '/rzd' : 'rzd_message(message)', '/weather' : 'weather_message(message)'}
+token=load_dotenv()
+token = os.getenv('TOKEN')
+print(token)
+
+commandlist = {'/start': 'start_message(message)', '/help' : 'help_message(message)', '/rzd' : 'rzd_message(message)', '/weather' : 'weather_message(message)', '/music' : 'music_message(message)'}
 
 lovestickerpack = ['CAADAgAD-wUAAtJaiAEK_F4c8hn9yxYE', 'CAADAgADcgkAAgi3GQIEU9tYxpNH9xYE', 'CAADAgADEgYAAtJaiAH3r7K1PEN3dBYE', 'CAADAgADgQkAAgi3GQJMZcFWk15u8RYE', 'CAADAgADgwkAAgi3GQKYlDU84Ixx3RYE', 'CAADAgADGgYAAtJaiAEu2wLZUu4NEBYE', 'CAADAgADBQADwDZPE_lqX5qCa011FgQ', 'CAADAgADFQADwDZPE81WpjthnmTnFgQ', 'CAADAgADBgADwDZPE8fKovSybnB2FgQ', 'CAADAgADFgADwDZPE2Ah1y2iBLZnFgQ', 'CAADAgADDQADwDZPE6T54fTUeI1TFgQ', 'CAADAgAD0wADVp29CvUyj5fVEvk9FgQ']
 
@@ -27,43 +25,40 @@ loadstickerpack = ['CAADAgADjwADFkJrCr24snHVnwbiFgQ', 'CAADAgADGAADwDZPE9b6J7-ca
 angrystickerpack = ['CAADAgADywADVp29CllGpcs9gzQoFgQ', 'CAADAgADIAADwDZPE_QPK7o-X_TPFgQ', 'CAADAgADfQAD9wLIDy7JuwrdyyJJFgQ', 'CAADAgADnwADFkJrCg3fpq5eaUiCFgQ']
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
-keyboard1.row('/start', '/help', '/weather', '/rzd', 'РљР°Р·Р°РЅСЊ', 'РњРѕСЃРєРІР°', '30.11.2019')
+keyboard1.row('/start', '/help', '/weather', '/rzd', 'Санкт-Петербург', 'Казань', '5.12.2019')
+keyboard1.row('/music')
 
 owm = pyowm.OWM('6d00d1d4e704068d70191bad2673e0cc', language = 'ru')
-Token = '992699822:AAFELsU-hkBdSSqzTln_lCLpldaBRecvcSE'
-bot = telebot.TeleBot(Token)
+bot = telebot.TeleBot(token)
 
 fromplace = str()
 toplace = str()
 dateregistration = str()
-status:str
+status=''
 
 @bot.message_handler(commands=['rzd'])
 
 def rzd_message(message):
-    bot.send_message(message.chat.id, 'Р’РІРµРґРёС‚Рµ РіРѕСЂРѕРґ РѕС‚РїСЂР°РІР»РµРЅРёСЏ')
+    bot.send_message(message.chat.id, 'Введите город отправления')
     bot.register_next_step_handler(message, from_place_registration)
-    
 def from_place_registration(message):
     global commandlist
     global fromplace
     if message.text not in commandlist:
         fromplace = message.text
-        bot.send_message(message.chat.id, 'Р’РІРµРґРёС‚Рµ РіРѕСЂРѕРґ РЅР°Р·РЅР°С‡РµРЅРёСЏ')
+        bot.send_message(message.chat.id, 'Введите город назначения')
         bot.register_next_step_handler(message, to_place_registration)
     else:
         exec(commandlist[message.text])
-    
 def to_place_registration(message):
     global commandlist
     global toplace
     if message.text not in commandlist:
         toplace = message.text
-        bot.send_message(message.chat.id, 'Р’РІРµРґРёС‚Рµ РґР°С‚Сѓ РѕС‚РїСЂР°РІР»РµРЅРёСЏ')
+        bot.send_message(message.chat.id, 'Введите дату отправления')
         bot.register_next_step_handler(message, date_registration)
     else:
         exec(commandlist[message.text])
-    
 def date_registration(message):
     global commandlist
     global fromplace
@@ -73,9 +68,12 @@ def date_registration(message):
     if message.text not in commandlist:
         dateregistration = message.text
         bot.send_sticker(message.chat.id, random.choice(loadstickerpack))
+        bot.send_message(message.chat.id, 'Ищу билеты по выбранному направлению')
+        print(fromplace)
+        print(toplace)
+        print(dateregistration)
         Sendler(fromInput=fromplace,fromOutput=toplace,date=dateregistration).send()
-        a=time.time()
-        bot.send_message(message.chat.id, "Р–РµР»РµР·РЅРѕРґРѕСЂРѕР¶РЅС‹Рµ РјР°СЂС€СЂСѓС‚С‹ РїРѕ РІР°С€РёРј С‚СЂРµР±РѕРІР°РЅРёСЏРј:" + str(reader.read()))
+        bot.send_message(message.chat.id, 'Железнодорожные маршруты по вашим требованиям: '+"\n"+reader.read())
     else:
         exec(commandlist[message.text])    
     
@@ -83,18 +81,17 @@ def date_registration(message):
 @bot.message_handler(commands=['start'])
 
 def start_message(message):
-    global weatherinformation
     global lovestickerpack
-    bot.send_message(message.chat.id, 'РџСЂРёРІРµС‚!\nРњРµРЅСЏ Р·РѕРІСѓС‚...РїР»РµРІР°С‚СЊ, СЏ Р¶Рµ С‚РµСЃС‚РѕРІС‹Р№ Р±РѕС‚\nР’РѕС‚ СЃРїРёСЃРѕРє РјРѕРёС… С„СѓРЅРєС†РёР№ РЅР° РґР°РЅРЅС‹Р№ РјРѕРјРµРЅС‚:\n1./start\n2./weather\n3./help\n4./rzd', reply_markup=keyboard1)
+    bot.send_message(message.chat.id, 'Привет!\nМеня зовут...плевать, я же тестовый бот\nВот список моих функций на данный момент:\n1./start\n2./weather\n3./help\n4./rzd', reply_markup=keyboard1)
     bot.send_sticker(message.chat.id, random.choice(lovestickerpack))
     
 @bot.message_handler(commands=['weather'])
 
 def weather_message(message):
-    bot.send_message(message.chat.id, 'РќСѓ РґР°РІР°Р№')
+    bot.send_message(message.chat.id, 'Ну давай')
     bot.register_next_step_handler(message, weather_information)
 def weather_information(message):
-    place:str
+    place=''
     global status
     global angrystickerpack
     if message.text not in commandlist:
@@ -106,17 +103,17 @@ def weather_information(message):
             temp = weather.get_temperature('celsius')['temp']
             wind = weather.get_wind()['speed']
             print(weather)
-            bot.send_message(message.from_user.id, "РџРѕРіРѕРґР° РіРѕСЂРѕРґР° " + message.text + "\nРўРµРјРїРµСЂР°С‚СѓСЂР°: " + str(temp) + "В°C" + "\nРќР° СѓР»РёС†Рµ: " + str.title(status) + "\nРЎРєРѕСЂРѕСЃС‚СЊ Р’РµС‚СЂР°: " + str(wind) + "Рј/c")
+            bot.send_message(message.chat.id, "Погода города " + message.text + "\nТемпература: " + str(temp) + "°C" + "\nНа улице: " + str.title(status) + "\nСкорость Ветра: " + str(wind) + "м/c")
             if temp >= 15:
-                bot.send_message(message.from_user.id, "РџРѕРіРѕРґР°-mood: CР°РјРѕРµ-С‚Рѕ ")
+                bot.send_message(message.chat.id, "Погода-mood: Cамое-то ")
             elif 15 > temp  and temp > 0:
-                bot.send_message(message.from_user.id, "РџРѕРіРѕРґР°-mood: РќР°РєРёРЅСЊ С‡С‚Рѕ РЅРёР±СѓРґСЊ РЅР° СЃРµР±СЏ ")
+                bot.send_message(message.chat.id, "Погода-mood: Накинь что нибудь на себя ")
             elif temp < 0 and -25 < temp:
-                bot.send_message(message.from_user.id, "РџРѕРіРѕРґР°-mood: РћРґРµРІР°Р№СЃСЏ РјР°С‚СЊ, РїРѕСЂР° РІРѕРµРІР°С‚СЊ ")
+                bot.send_message(message.chat.id, "Погода-mood: Одевайся мать, пора воевать ")
             elif temp <= -25:
-                bot.send_message(message.from_user.id, "РџРѕРіРѕРґР°-mood: РўС‹ СѓРјСЂС‘С€СЊ, РµСЃР»Рё СѓР№РґС‘С€СЊ")
+                bot.send_message(message.chat.id, "Погода-mood: Ты умрёшь, если уйдёшь")
         except pyowm.exceptions.api_response_error.NotFoundError:
-            bot.reply_to(message, 'Р’СЂРµС€СЊ, С‚Р°РєРѕРіРѕ РіРѕСЂРѕРґР° РЅРµС‚ РЅР° РєР°СЂС‚Р°С…')
+            bot.reply_to(message, 'Врешь, такого города нет на картах')
             bot.send_sticker(message.chat.id, random.choice(angrystickerpack))
     else:
         exec(commandlist[message.text])
@@ -124,21 +121,29 @@ def weather_information(message):
 @bot.message_handler(commands=['help'])
 def help_message(message):
     global lovestickerpack
-    bot.send_message(message.chat.id, '1./start - СЌС‚Р° С„СѓРЅРєС†РёСЏ РїРѕР·РІРѕР»СЏРµС‚ Р’Р°Рј СЃР±СЂРѕСЃРёС‚СЊ РЅР°С€ РґРёР°Р»РѕРі Рё РІРµСЂРЅСѓС‚СЊСЃСЏ Рє РёСЃС…РѕРґРЅРѕР№ С‚РѕС‡РєРµ\n2./weather - РїРѕР·РІРѕР»СЏРµС‚ РІР°Рј СѓР·РЅР°С‚СЊ СЃРѕСЃС‚РѕСЏРЅРёРµ РїРѕРіРѕРґС‹ РІ РґР°РЅРЅРѕРј РјРµСЃС‚Рµ\n3./help - СЌС‚Р°  С„СѓРЅРєС†РёСЏ СЃСЂР°Р±РѕС‚Р°Р»Р° РїСЂСЏРјРѕ СЃРµР№С‡Р°СЃ')
-    bot.send_sticker(random.choice(lovestickerpack))
+    bot.send_message(message.chat.id, '1./start - эта функция позволяет Вам сбросить наш диалог и вернуться к исходной точке\n2./weather - позволяет вам узнать состояние погоды в данном месте\n3./help - эта  функция сработала прямо сейчас')
+    bot.send_sticker(message.chat.id,random.choice(lovestickerpack))
     
+@bot.message_handler(commands=['music'])
+def music_message(message):
+    for i in range(3):
+        n=random.randint(1,2)
+        audio = open(str(n)+".mp3", mode='rb')
+        print("opened "+str(n)+".mp3")
+        bot.send_audio(message.from_user.id,audio, timeout=1000)  
 @bot.message_handler(content_types=['text'])
+
 def text_analyze(message):
     global lovestickerpack
     global angrystickerpack
     global questionstickerpack
-    if 'СЂСѓСЃС‚Р°Рј' in message.text.lower():
-        bot.reply_to(message, 'РІ РјРѕРµР№ СЃРёСЃС‚РµРјРµ СЂРµР№С‚РёРЅРіР° "Р СѓСЃС‚Р°Рј" СЃС‚РѕРёС‚ РЅР° РїРµСЂРІРѕРј РјРµСЃС‚Рµ')
+    if 'рустам' in message.text.lower():
+        bot.reply_to(message, 'в моей системе рейтинга "Рустам" стоит на первом месте, если не считать всех других членов команды')
         bot.send_sticker(message.chat.id, random.choice(lovestickerpack))
-    elif 'Р°СЂРёРЅР°' in message.text.lower() or 'Р°СЂРёС€Р°' in message.text.lower():
-        bot.reply_to(message, 'Р°СЂРёРЅР°...Р°СЂРёС€Р°...Р·РІСѓС‡РёС‚ РєР°Рє С‡С‚Рѕ-С‚Рѕ РЅРµРїСЂРёСЏС‚РЅРѕРµ')
+    elif 'арина' in message.text.lower() or 'ариша' in message.text.lower():
+        bot.reply_to(message, 'арина...ариша...звучит как что-то неприятное')
         bot.send_sticker(message.chat.id, random.choice(angrystickerpack))
     elif message.text.lower():
-        bot.reply_to(message, 'СЏ РїРѕРєР° РЅРµ РІ СЃРѕСЃС‚РѕСЏРЅРёРё РїРѕРЅСЏС‚СЊ С‚РІРѕРё РјС‹СЃР»Рё')
+        bot.reply_to(message, 'я пока не в состоянии понять твои мысли')
         bot.send_sticker(message.chat.id, random.choice(questionstickerpack))
 bot.polling()
